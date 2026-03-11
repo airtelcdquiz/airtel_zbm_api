e0c365a267d832ca55fa1f3714a2cfeac25c2827
airtelcdquiz <airtel.cd.quiz@gmail.com>
2026-03-11 13:35:12 +0000

    je finxe user

--- models/document.py
+++ models/document.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, JSON
 from sqlalchemy.orm import relationship
 from .database import db 
+from .user import User
 
 class Document(db.Model):
     __tablename__ = 'documents'
@@ -27,7 +28,7 @@ class Document(db.Model):
     processing_result = Column(JSON)  # Résultats détaillés du traitement
 
     # Relations
-    user = relationship('User', backref='documents', foreign_keys=[uploaded_by])
+    user = relationship(User, backref='documents', foreign_keys=[uploaded_by])
 
     def __repr__(self):
         return f'<Document {self.name}>'
