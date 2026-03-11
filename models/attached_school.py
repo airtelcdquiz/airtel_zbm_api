5d89000e458b3e7ace2f8f1925c7d43e8404b50b
airtelcdquiz <airtel.cd.quiz@gmail.com>
2026-03-11 11:39:16 +0000

    fixer un proble d'importation

--- models/attached_school.py
+++ models/attached_school.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from .database import db
 
-from models.school import School
-from models.attached_school import AttachedSchool
-from models.user import User
+from .school import School 
+from .user import User
 
 class AttachedSchool(db.Model):
     __tablename__ = 'attached_schools'
